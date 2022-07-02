37e10184237d2231d28444891df7150d5feb5079
collins felix <cllnsflx40@gmail.com>
2022-07-02 15:26:32 -0700

    deploying the frontend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API = axios.create({ baseURL: 'http://localhost:5000' })
+const API = axios.create({ baseURL: 'https://facebookclonetechsavvy.herokuapp.com' })
 
 export const getposts = (page) => API.get(`/posts?page=${page}`);
 export const createpost = (newPost) => API.post('/posts', newPost);
